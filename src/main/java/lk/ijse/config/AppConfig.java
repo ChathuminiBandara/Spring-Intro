fa5e773f7f49cd2112384c8a675c68a115941455
ChathuminiBandara <chathubandara500@gmail.com>
2024-08-24 12:34:23 +0530

    Configuring Types & Adding the BeanLifeCycle

--- src/main/java/lk/ijse/config/AppConfig.java
+++ src/main/java/lk/ijse/config/AppConfig.java
@@ -1,6 +1,7 @@
 package lk.ijse.config;
 
 
+import lk.ijse.obj.BeanLifeCycle;
 import lk.ijse.obj.MyObj;
 import lk.ijse.obj.OtherObj;
 import org.springframework.context.annotation.Bean;
@@ -9,20 +10,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 
-/*1st way*/
+/*1st way -- spring wlata eligible wenna puluwn widiya 1*/
 
 
 /*spring walata kiyanawa base package ekai sub pakages okkomai balanna kiyala*/
-@ComponentScan(basePackages = "lk.ijse")
+//@ComponentScan(basePackages = "lk.ijse")
 
 /*spring walata balanna ona than tika witharai kiyanne*/
-//@ComponentScan(basePackageClasses = {MyObj.class})
+@ComponentScan(basePackageClasses = {MyObj.class ,BeanLifeCycle.class})
 
 public class AppConfig {
 
-    /*2nd way*/
+    /*2nd way -- spring wlata eligible wenna puluwn widiya 2*/
     @Bean
     OtherObj otherObj(){
+        /*spring wala ena dulaba moment ekak "new" keyword eka use krana*/
         return new OtherObj();
     }
 }
